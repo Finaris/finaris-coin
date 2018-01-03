342db53efe3c5a0e2e006d5415afcf86dd19a826
Finaris <metorres@mit.edu>
2018-01-02 23:34:36 -0800

    Added logic for adding to a BlockChain, as well as sanity checking input.

--- src/com/company/BlockChain.java
+++ src/com/company/BlockChain.java
@@ -1,17 +1,57 @@
 package com.company;
 
+import java.lang.reflect.MalformedParametersException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
- * Class which contains the infrastructure of a BlockChain and its associated supporting methods.
+ * Class which contains the infrastructure of a block chain and its associated supporting methods.
  */
 
 public class BlockChain {
 
-    private ArrayList<Block> chain = new ArrayList<>();
+    // HashMap of Block hashes to Blocks will be internal structure.
+    private HashMap<String, Block> chain = new HashMap<>();
 
     public BlockChain() {}
 
+    /** Determines if the new block we wish to add results in a valid block chain.
+     *  This includes checking for: a valid index, a proper previousHash pointer, and a proper hash.
+     *
+     * @param newBlock Block which we are adding in reference to previousBlock.
+     * @param previousBlock Block which we are adding to.
+     * @return Boolean determining if a provided block can be validly added to the block chain.
+     */
+    public boolean isValidNewBlock(Block newBlock, Block previousBlock) {
+        /* Check to make sure that the index is one greater and that the previousHash in the newBlock is the same
+         * as the hash of the previousBlock. */
+        if (previousBlock.getIndex() != newBlock.getIndex() - 1 ||
+                !previousBlock.getHash().equals(newBlock.getPreviousHash()))
+            return false;
 
+        // Sanity check for a proper hash on the newBlock.
+        return BlockUtilities.hashBlock(newBlock.getIndex(), newBlock.getData(), newBlock.getTimestamp(),
+                newBlock.getPreviousHash()).equals(newBlock.getHash());
+    }
+
+    /** Adds a new block to the block chain.
+     *
+     * @param block Block to add.
+     */
+    public void add(Block block) {
+        // Define special behavior for a genesis block.
+        if (chain.size() == 0 && block.getPreviousHash() == null && block.getIndex() == 0) {
+            chain.put(block.getHash(), block);
+        } else if (chain.size() > 0) {
+            /* First check to make sure that a block with the previous hash exists in the chain. Then, check
+             * to make sure that it can properly be added to the chain. */
+            if (!chain.containsKey(block.getPreviousHash()) ||
+                    !isValidNewBlock(block, chain.get(block.getPreviousHash())))
+                throw new MalformedParametersException();
+            chain.put(block.getHash(), block);
+        } else {
+            throw new MalformedParametersException();
+        }
+    }
 
 }
--- src/com/company/BlockChainTest.java
+++ src/com/company/BlockChainTest.java
@@ -5,7 +5,34 @@ import org.junit.Test;
 
 public class BlockChainTest {
 
-    // Creates genesis block with data "TEST" at timestamp 123456789 seconds.
-    // Block genesisBlock = new Block(0, "TEST", 123456789, , null);
+    @Test
+    public void testAdd() {
+        // Test the functionality of add() as well as isValidNewBlock() on a BlockChain object.
+        BlockChain chain = new BlockChain();
+
+        // Creates genesis block with data "TEST" at timestamp 123456789 seconds and add to chain.
+        String genesisHash = BlockUtilities.hashBlock(0, "TEST", 123456789, null);
+        Block genesisBlock = new Block(0, "TEST", 123456789, genesisHash, null);
+        chain.add(genesisBlock);
+
+        // Check if index is invalid on a block with index 2 and data "BAR" at timestamp 234567891 seconds.
+        String newBlockHash = BlockUtilities.hashBlock(2, "BAR", 234567891, genesisHash);
+        Block newBlock = new Block(2, "BAR", 234567891, newBlockHash, genesisHash);
+        Assert.assertFalse(chain.isValidNewBlock(genesisBlock, newBlock));
+
+        // Check if index is invalid on the same block with a corrected index, but with an incorrect previous hash.
+        newBlockHash = BlockUtilities.hashBlock(1, "BAR", 234567891, genesisHash);
+        newBlock = new Block(1, "BAR", 234567891, newBlockHash, newBlockHash);
+        Assert.assertFalse(chain.isValidNewBlock(genesisBlock, newBlock));
+
+        // Check if index is invalid on the same block with a corrected index, but with a malformed hash.
+        newBlock = new Block(1, "BAR", 234567891, newBlockHash, "null");
+        Assert.assertFalse(chain.isValidNewBlock(genesisBlock, newBlock));
+
+        // Add a correctly formed newBlock.
+        newBlockHash = BlockUtilities.hashBlock(1, "BAR", 234567891, genesisHash);
+        newBlock = new Block(1, "BAR", 234567891, newBlockHash, genesisHash);
+        chain.add(newBlock);
+    }
 
 }
